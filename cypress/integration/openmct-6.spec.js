d0580f6040659b382ef5f67ef5f4e4337c96785d
John Hill <johill@redhat.com>
2021-02-22 08:32:35 -0500

    linting fix

--- cypress/integration/openmct-6.spec.js
+++ cypress/integration/openmct-6.spec.js
@@ -1,45 +1,43 @@
-/** 
- * 
+/**
+ *
  * Requirement Description:
  *  The application shall maintain the user-selected table sort order at all times
  *  TODO Replace momentjs with dayjs
- * 
+ *
  **/
 
 describe('OPENMCT-6 Functional', () => {
-    
-    it('OPENMCT-6-F001 - The application shall maintain the user-selected table sort after toggling telemetry points', () => {
-        cy.intercept('GET', '/sockjs-node/info?t=**').as('loaded')
-        cy.visit('/')
-        cy.wait(['@loaded','@loaded'])
+  it('OPENMCT-6-F001 - The application shall maintain the user-selected table sort after toggling telemetry points', () => {
+    cy.intercept('GET', '/sockjs-node/info?t=**').as('loaded')
+    cy.visit('/')
+    cy.wait(['@loaded', '@loaded'])
 
-        cy.get('.timestamp-sort').should('be.visible')
-        cy.checkTableFirstAndLastValue(15)
-        cy.get('.timestamp-sort').click()
-        cy.checkTableFirstAndLastValue(-15)
-        cy.get('[value="pwr.v"]').click()
-        cy.checkTableFirstAndLastValue(-15)
-        cy.get('[value="pwr.c"]').click()
-        cy.checkTableFirstAndLastValue(-15)
-    })
+    cy.get('.timestamp-sort').should('be.visible')
+    cy.checkTableFirstAndLastValue(15)
+    cy.get('.timestamp-sort').click()
+    cy.checkTableFirstAndLastValue(-15)
+    cy.get('[value="pwr.v"]').click()
+    cy.checkTableFirstAndLastValue(-15)
+    cy.get('[value="pwr.c"]').click()
+    cy.checkTableFirstAndLastValue(-15)
+  })
 })
 
 describe('OPENMCT-6 Non-functional', () => {
-    it('OPENMCT-6-NF001 - The application shall maintain the user-selected table sort order after reload', () => {
-        //Note: this test will fail due to the lack of persistent state
+  it('OPENMCT-6-NF001 - The application shall maintain the user-selected table sort order after reload', () => {
+    //Note: this test will fail due to the lack of persistent state
+    cy.intercept('GET', '/sockjs-node/info?t=**').as('loaded')
+    cy.visit('/')
+    cy.wait(['@loaded', '@loaded'])
 
-        cy.intercept('GET', '/sockjs-node/info?t=**').as('loaded')
-        cy.visit('/')
-        cy.wait(['@loaded','@loaded'])
-
-        cy.get('.timestamp-sort').should('be.visible').and('have.class', 'desc')
-        cy.checkTableFirstAndLastValue(15)
-        cy.get('.timestamp-sort').should('have.class', 'desc').click()
-        cy.checkTableFirstAndLastValue(-15)
-        cy.get('.timestamp-sort').should('have.class', 'asc')
-        cy.reload()
-        cy.wait(['@loaded','@loaded'])
-        cy.get('.timestamp-sort').should('have.class', 'asc') //This will fail
-        cy.checkTableFirstAndLastValue(-15) //This will fail
-    })
+    cy.get('.timestamp-sort').should('be.visible').and('have.class', 'desc')
+    cy.checkTableFirstAndLastValue(15)
+    cy.get('.timestamp-sort').should('have.class', 'desc').click()
+    cy.checkTableFirstAndLastValue(-15)
+    cy.get('.timestamp-sort').should('have.class', 'asc')
+    cy.reload()
+    cy.wait(['@loaded', '@loaded'])
+    cy.get('.timestamp-sort').should('have.class', 'asc') //This will fail
+    cy.checkTableFirstAndLastValue(-15) //This will fail
+  })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,29 +30,29 @@ import moment from 'moment'
 //The intent of this function is to verify that the first and last value of the table are
 // expectedMinutes apart.
 Cypress.Commands.add('checkTableFirstAndLastValue', (exptectedMinutes) => {
-    cy.get('[class="content-body"]').then((tbody) => {
-        const first_record = tbody.find('tr:first>td').eq(1) //TODO optimize
-        const last_record = tbody.find('tr:last>td').eq(1)  //TODO optimize
-        
-        cy.log('Verify Table Timestample values')
-        const first_timestamp = moment(first_record[0].innerText) //i.e. "2021-02-20T03:32:48.749Z"
-        const last_timestamp = moment(last_record[0].innerText) //i.e. "2021-02-20T03:17:49.490Z"
-        cy.log('Table first Timestamp value=' + first_timestamp)
-        cy.log('Table last Timestamp value=' + last_timestamp)
-        
-        const duration = Math.round(first_timestamp.diff(last_timestamp,"minutes",true))
-        cy.log(duration + ' minutes between first and last Timestamp')
-        expect(duration).to.deep.equal(exptectedMinutes)
-    })
+  cy.get('[class="content-body"]').then((tbody) => {
+    const first_record = tbody.find('tr:first>td').eq(1) //TODO optimize
+    const last_record = tbody.find('tr:last>td').eq(1) //TODO optimize
+
+    cy.log('Verify Table Timestample values')
+    const first_timestamp = moment(first_record[0].innerText) //i.e. "2021-02-20T03:32:48.749Z"
+    const last_timestamp = moment(last_record[0].innerText) //i.e. "2021-02-20T03:17:49.490Z"
+    cy.log('Table first Timestamp value=' + first_timestamp)
+    cy.log('Table last Timestamp value=' + last_timestamp)
+
+    const duration = Math.round(first_timestamp.diff(last_timestamp, 'minutes', true))
+    cy.log(duration + ' minutes between first and last Timestamp')
+    expect(duration).to.deep.equal(exptectedMinutes)
+  })
 })
 
 //The intent of this function is to verify that the table exists but is missing values
 Cypress.Commands.add('verifyEmptyTable', () => {
-    cy.get('[class="content-body"]').then((tbody) => {
-        const first_record = tbody.find('tr:first>td').eq(1) //TODO optimize
-        const last_record = tbody.find('tr:last>td').eq(1)  //TODO optimize
-        expect(first_record.length).to.eq(0)
-        expect(last_record.length).to.eq(0)
-        cy.log('Verified Empty Table Timestamp values')  
-    })
+  cy.get('[class="content-body"]').then((tbody) => {
+    const first_record = tbody.find('tr:first>td').eq(1) //TODO optimize
+    const last_record = tbody.find('tr:last>td').eq(1) //TODO optimize
+    expect(first_record.length).to.eq(0)
+    expect(last_record.length).to.eq(0)
+    cy.log('Verified Empty Table Timestamp values')
+  })
 })
